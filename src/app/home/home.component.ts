2c625ec49b8eb8dfa0e83ac496f656b272420c4d
DDT13163 <101248065+DDT13163@users.noreply.github.com>
2023-02-01 10:49:54 +0700

    ngFor dung cho Object

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   traiCay = ['Táo', 'Nho', 'Cam', 'Quýt', 'Xoài', 'Ổi'];
   theThao = ['Bóng Đá', 'Bóng Chuyền', 'Bóng Rổ', 'Quần Vợt', 'Bơi'];
+  traiCay2 = [{ ten: "Táo", gia: 12, haGia: true }, { ten: "Nho", gia: -3, haGia: false }, { ten: "Xoài", gia: 34.5, haGia: true }];
   constructor() { }
 
   ngOnInit(): void {
